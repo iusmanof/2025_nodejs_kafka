8d110f798bf143a5f2ed5219fb8cb3848269a8e1
iusmanof <iusmanof@gmail.com>
2025-08-25 22:58:06 +0300

    feat: code complete

--- config.js
+++ config.js
@@ -1,43 +1,43 @@
 import kafka from "kafkajs";
 
-class kafkaConfig{
+class kafkaConfig {
 
-    constructor(){
+    constructor() {
         this.kafka = new kafka({
             clientId: "nodejs-kafka",
             brokers: ['localhost:9093'],
         })
         this.producer = this.kafka.producer();
-        this.consumer = this.kafka.consumer({groupId: 'test-group'});
+        this.consumer = this.kafka.consumer({ groupId: 'test-group' });
 
     }
 
 
-    async produce(topic, messages){
-        try{
+    async produce(topic, messages) {
+        try {
             await this.producer.connect()
             await this.producer.send({
                 topic: topic,
                 messages: messages
             })
-        }catch (err){
+        } catch (err) {
             console.log(err)
-        } finally{
+        } finally {
             await this.producer.disconnect()
         }
     }
 
-    async consume(topic,callback){
-        try{
+    async consume(topic, callback) {
+        try {
             await this.consumer.connect()
-            await this.consumer.subscribe({topic: topic, fromBeginning: true})
+            await this.consumer.subscribe({ topic: topic, fromBeginning: true })
             await this.consumer.run({
-                eachMessage: async({topic, partition, message}) => {
+                eachMessage: async ({ topic, partition, message }) => {
                     const value = message.value.toString()
                     callback(value)
                 }
-         })
-        } catch (err){
+            })
+        } catch (err) {
             console.log(err)
         }
     }
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import bodyParser from "body-parser";
 import express from "express";
+import controllers from "./ontroller";
+import kafkaConfig from "./config";
 
 const app = express()
 const jsonParser = bodyParser.json();
+
+app.post('/api/send', jsonParser, controllers.sendMessageToKafka)
+
+const kafkaConfig = new kafkaConfig()
+kafkaConfig.consume('my-topic', (value) => {
+    console.log(value)
+})
 
 app.listen(8080, () => {
     console.log("server stgart on 8080 port...")
