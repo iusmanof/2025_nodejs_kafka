f05ce3dcc4be4ef1205350540f7d4f4493975d0c
iusmanof <iusmanof@gmail.com>
2025-08-25 22:43:18 +0300

    feat: create kafka config

---
+++ config.js
@@ -0,0 +1,46 @@
+import kafka from "kafkajs";
+
+class kafkaConfig{
+
+    constructor(){
+        this.kafka = new kafka({
+            clientId: "nodejs-kafka",
+            brokers: ['localhost:9093'],
+        })
+        this.producer = this.kafka.producer();
+        this.consumer = this.kafka.consumer({groupId: 'test-group'});
+
+    }
+
+
+    async produce(topic, messages){
+        try{
+            await this.producer.connect()
+            await this.producer.send({
+                topic: topic,
+                messages: messages
+            })
+        }catch (err){
+            console.log(err)
+        } finally{
+            await this.producer.disconnect()
+        }
+    }
+
+    async consume(topic,callback){
+        try{
+            await this.consumer.connect()
+            await this.consumer.subscribe({topic: topic, fromBeginning: true})
+            await this.consumer.run([
+                eachMessage: async({topic, partition, message}) => {
+                    const value = message.value.toString()
+                    callback(value)
+                }
+            ])
+        } catch (err){
+            console.log(err)
+        }
+    }
+}
+
+export default kafkaConfig
